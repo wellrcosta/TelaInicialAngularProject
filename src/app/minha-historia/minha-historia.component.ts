874532b29257be7ae0fa24dd520126c410d6f3b6
Otavio <otaviobubans@hotmail.com>
2019-05-23 18:06:02 -0300

    Edit #9 - Minha história

--- src/app/minha-historia/minha-historia.component.ts
+++ src/app/minha-historia/minha-historia.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-minha-historia',
   templateUrl: './minha-historia.component.html',
-  styleUrls: ['./minha-historia.component.css']
+  styleUrls: ['../noticias/bootstrap.component.css', './minha-historia.component.css']
 })
 export class MinhaHistoriaComponent implements OnInit {
 
