39af127f41cfa472fafce1faecb825863be8b582
Wellington Reis <wellington.costa.97@gmail.com>
2019-05-27 16:12:24 -0300

    Edit #11
    Aniversariantes adicionados. Cor do fundo corrigida e outras alterações de layout

--- src/app/novos-colaboradores/novos-colaboradores.component.ts
+++ src/app/novos-colaboradores/novos-colaboradores.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-novos-colaboradores',
   templateUrl: './novos-colaboradores.component.html',
-  styleUrls: ['./novos-colaboradores.component.css']
+  styleUrls: ['./novos-colaboradores.component.css', '../../assets/css/bootstrap.component.css']
 })
 export class NovosColaboradoresComponent implements OnInit {
 
