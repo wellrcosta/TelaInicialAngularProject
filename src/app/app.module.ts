0e4b853f77defecbd4671e22ea7d65de2ea307f5
Wellington Reis <wellington.costa.97@gmail.com>
2019-05-31 14:52:05 -0300

    Edit #13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AniversariosComponent } from './aniversarios/aniversarios.component';
 import { MinhaHistoriaComponent } from './minha-historia/minha-historia.component';
 import { NovosColaboradoresComponent } from './novos-colaboradores/novos-colaboradores.component';
 import { SkaInterligadaComponent } from './ska-interligada/ska-interligada.component';
+import {FooterTargetComponent} from './footer-target/footer-target.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { SkaInterligadaComponent } from './ska-interligada/ska-interligada.compo
     AniversariosComponent,
     MinhaHistoriaComponent,
     NovosColaboradoresComponent,
-    SkaInterligadaComponent
+    SkaInterligadaComponent,
+    FooterTargetComponent
   ],
   imports: [
     BrowserModule,
